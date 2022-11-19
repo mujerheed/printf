3cfe261f4d2ddd5827222cc0558b3fd2a216f226
Abdurrazaq Jibril Baba <49338500+mujerheed@users.noreply.github.com>
2022-11-20 00:55:08 +0100

    printf project task updated

--- _printf.c
+++ _printf.c
@@ -27,3 +27,38 @@ int _printf(const char *format, ...)
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
 /* write(1, &format[i], 1);*/
+printed_chars++;
+}
+else
+{
+print_buffer(buffer, &buff_ind);
+flags = get_flags(format, &i);
+width = get_width(format, &i, list);
+precision = get_precision(format, &i, list);
+size = get_size(format, &i);
+i++;
+printed = handle_print(format, &i, list, buffer,
+flags, width, precision, size);
+if (printed == -1)
+return (-1);
+printed_chars += printed;
+}
+}
+
+print_buffer(buffer, &buff_ind);
+
+va_end(list);
+
+return (printed_chars);
+}
+
+/**
+* print_buffer - Prints the contents of the buffer if it exists
+* @buffer: Array of chars
+* @buff_ind: Index at which to add next char, represents the length
+*/
+void print_buffer(char buffer[], int *buff_ind)
+{
+if (*buff_ind > 0)
+write(1, &buffer[0], *buff_ind);
+}
