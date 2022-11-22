6a2f1ee4980d13e7aa2444e65a7befa8ac5a324e
Abdurrazaq Jibril <mujerheed20@gmail.com>
2022-11-22 08:30:12 +0000

    Update printf project

---
+++ get_width.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* get_width - Calculates the width for printing
+* @format: Formatted string in which to print the arguments.
+* @i: List of arguments to be printed.
+* @list: list of arguments.
+*
+* Return: width.
+*/
+int get_width(const char *format, int *i, va_list list)
+{
+	int index;
+	int width = 0;
+
+	for (index = *i + 1; format[index] != '\0'; index++)
+	{
+		if (is_digit(format[index]))
+		{
+			width *= 10;
+			width += format[index] - '0';
+		}
+		else if (format[index] == '*')
+		{
+			curr_i++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = index - 1;
+
+	return (width);
+}
